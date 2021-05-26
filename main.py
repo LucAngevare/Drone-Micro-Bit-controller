a78c0e0078e922cb767c525479fd25e9bb0ee837
Luc Angevare <lucangevare@gmail.com>
2021-05-26 11:51:52 +0200

    Finished the project
    Now waiting for the WiFi to connect and the micro:bit to act connected, if it's connected before the Raspberry Pi is booted it just treats it like a charging device, while I need it to treat it like a filesystem and serial host. Forcing user to un-/replug the micro:bit to work.

--- main.py
+++ main.py
@@ -1,54 +1,83 @@
-import os 
+import subprocess
+import os
 import time
-import threading 
+import threading
+import shutil
 import socket
 import sys
 import time
 import chardet
-from datetime import datetime 
-from serial import Serial 
+from datetime import datetime
+from serial import Serial
 
 host = ''
 port = 9000
-locaddr = (host,port) 
+locaddr = (host,port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tello_address = ('192.168.10.1', 8889)
 sock.bind(locaddr)
 nextCompassPoll = 0.0 ;
 serialDevDir='/dev/serial/by-id'
 string = ""
+doneMoveFile = False
+
+src = r"/home/pi/Desktop/Drone/micro:bit_hex.hex"
+destination = r"/media/pi/MICROBIT/code.hex"
+
+while (not os.path.isdir(serialDevDir)):
+    print("replug the micro:bit")
+    time.sleep(5)
+
+while (not doneMoveFile):
+    if (not os.path.isdir("/media/pi/MICROBIT")):
+        print("please replug the micro:bit hh")
+        time.sleep(5)
+    try:
+        shutil.copyfile(src, destination)
+        print("done the movement")
+        doneMoveFile = True
+    except:
+        print("Please replug the micro:bit h")
+        time.sleep(5)
 
 if ( os.path.isdir(serialDevDir) ):
-    serialDevices = os.listdir(serialDevDir) 
+    serialDevices = os.listdir(serialDevDir)
 
     if ( len(serialDevices) > 0 ):
 
         serialDevicePath = os.path.join(serialDevDir, serialDevices[0])
         print(serialDevicePath)
 
-        serial = Serial(port=serialDevicePath, baudrate=19200, timeout=None) 
+        serial = Serial(port=serialDevicePath, baudrate=19200, timeout=None)
 
         while( True ):
-            bytesToRead = serial.inWaiting()
-            receivedMsg = serial.read(bytesToRead)
-            if (receivedMsg):
-                string += "".join(str(receivedMsg.decode()))
-                if (";" in string):
-                    print("received a message")
-                    msgType = string.split(": ")[0].strip()
-                    msgData = "".join(string.split(": ")[1:])
-                    print(msgType)
-                    if (msgType.lstrip() == "DRONE"):
-                        msg = msgData.strip() #Ik ben er klaar mee, dit ga ik als bodge gebruiken, fuck dit
-                        print(msg)
-                        sock.sendto(b"command", tello_address)
-                        sent = sock.sendto(msg.replace(";", "").encode(), tello_address)
-                        print("sent message to drone")
-                    string = ""
+            try:
+                helpme = subprocess.check_output(["iwgetid", "-r"]).strip()
+                if (not helpme == b"TELLO-59FB9E"):
+                    time.sleep(10)
+                    print("not yet connected to wifi")
+                else:
+                    bytesToRead = serial.inWaiting()
+                    receivedMsg = serial.read(bytesToRead)
+                    if (receivedMsg):
+                        string += "".join(str(receivedMsg.decode(chardet.detect(receivedMsg).get("encoding"))))
+                        print(string)
+                        print(receivedMsg)
+                        if (";" in string):
+                            print("received a message")
+                            msgType = string.split(": ")[0].strip()
+                            msgData = "".join(string.split(": ")[1:])
+                            print(msgType)
+                            if (msgType.lstrip() == "DRONE"):
+                                msg = msgData.split(";")[0].strip()
+                                print(msg)
+                                sock.sendto(b"command", tello_address)
+                                sent = sock.sendto(msg.encode(), tello_address)
+                                print("sent message to drone")
+                            string = ""
+            except:
+                print("not yet connected to wifi")
+                time.sleep(10)
     else:
 
-        print('No serial devices connected') 
-
-else:
-
-    print(serialDevDir + ' does not exist') 
+        print('No serial devices connected')
--- micro:bit_code.js
+++ micro:bit_code.js
@@ -1,23 +1,40 @@
-input.onButtonPressed(Button.A, function () {
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    sendQueue.push("DRONE: land;")
+    takenoff = 0
     basic.showLeds(`
         . . # . .
-        . # . # .
-        . # # # .
-        . # . # .
-        . # . # .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
         `)
-    sendQueue.push("DRONE: takeoff;")
+})
+input.onButtonPressed(Button.A, function () {
+    if (takenoff) {
+        sendQueue.push("DRONE: up 50;")
+    } else {
+        sendQueue.push("DRONE: takeoff;")
+        takenoff = 1
+    }
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
+    sendQueue.push("DRONE: down 50;")
     basic.showLeds(`
-        . # # . .
-        . # . # .
+        . . # . .
+        . . # . .
+        # . # . #
         . # # # .
-        . # . # .
-        . # # . .
+        . . # . .
         `)
-    sendQueue.push("DRONE: land;")
 })
+let takenoff = 0
 let sendQueue: string[] = []
 serial.redirect(
 SerialPin.USB_TX,
@@ -25,8 +42,58 @@ SerialPin.USB_RX,
 BaudRate.BaudRate19200
 )
 basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        sendQueue.push("DRONE: back 40;")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . # .
+            . . # . .
+            `)
+    } else if (input.isGesture(Gesture.LogoDown)) {
+        sendQueue.push("DRONE: forward 40;")
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (input.isGesture(Gesture.TiltLeft)) {
+        sendQueue.push("DRONE: ccw 45;")
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (input.isGesture(Gesture.TiltRight)) {
+        sendQueue.push("DRONE: cw 45;")
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
     while (sendQueue.length > 0) {
         serial.writeLine("" + (sendQueue.shift()))
-        basic.showNumber(sendQueue.length)
     }
 })
